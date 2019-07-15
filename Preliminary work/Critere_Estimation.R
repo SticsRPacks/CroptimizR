b274dcb5ae505c1075da5d6627b4e863ebe34091
tvailhere <45198977+tvailhere@users.noreply.github.com>
2019-07-15 09:53:10 +0200

    Update Critere_Estimation.R
    Generic treatement of USM group
    Add "set_code_optim"

--- Preliminary work/Critere_Estimation.R
+++ Preliminary work/Critere_Estimation.R
@@ -18,8 +18,6 @@ library(nloptr)
 library(stringr)
 
 
-
-
 # ----
 crit <- function(sim,obs,flag_log){
   res <- 0
@@ -43,21 +41,30 @@ Stic_Crit <- function(param_value, param_name, USM, USM_list, obs_val, obs_jul, 
   sim_value <- vector("numeric")
   sim_inter <- vector("list")
   
-  print(param_value)
+  param_value_inter <- vector("numeric")
   for (i in 1:length(USM_list)){
     for (y in 1:length(USM)){
-      if (USM_list[i] %in% USM[[y]]){
-        param_name_inter<-append(param_name[1],param_name[y])
-        param_value_inter<-append(param_value[1],param_value[y])
-        gen_param_sti(paste0("D:/Home/Tvailhere/Interface/DonneesSticsCas1c/",USM_list[i]), param_name_inter, param_value_inter)
-        run_system("D:/Home/Tvailhere/Interface/JavaSTICS-1.41-stics-9.0/bin/stics_modulo", "D:/Home/Tvailhere/Interface/DonneesSticsCas1c", USM_list[i]) 
+      if (is.null(USM[[y]])==FALSE){
+        for (z in 1:length(USM[[y]])){
+          #print(USM[y])
+          if (USM_list[i] %in% USM[[y]][[z]]){
+            param_value_inter[y] <- param_value[[y]][z]
+          }
+        }
+      } else {
+        param_value_inter[y] <- param_value[[y]]
       }
     }
+    gen_param_sti(paste0("D:/Home/Tvailhere/Interface/DonneesSticsCas1c/", USM_list[i]), param_name, param_value_inter)
+    set_codeoptim(paste0("D:/Home/Tvailhere/Interface/DonneesSticsCas1c/", USM_list[i]))
+    run_system("D:/Home/Tvailhere/Interface/JavaSTICS-1.41-stics-9.0/bin/stics_modulo", "D:/Home/Tvailhere/Interface/DonneesSticsCas1c", USM_list[i]) 
+    
     sim_inter<-get_daily_results(paste0("D:/Home/Tvailhere/Interface/DonneesSticsCas1c/",USM_list[i]), USM_list[i], doy_list = obs_jul[[i]])
     sim_value <- append(sim_value, sim_inter$lai.n.)
   }
   
   res <- crit(sim_value, obs_val, flag_log)
+  print(param_value)
   print(res)
   return(res)
   
@@ -65,31 +72,72 @@ Stic_Crit <- function(param_value, param_name, USM, USM_list, obs_val, obs_jul, 
 
 
 # --- SCRIPT
+
+#TEST 1
+param_name <- c("dlaimax", "durvieF", "test1", "test2", "test3")
+lb <- list(0.0005, c(50, 40), 15, c(5, 100, 30), c(0, 0)) 
+ub <- list(0.0025, c(400, 500), 30, c(50, 500, 60), c(10, 15))
 # Tous les USM
-USM <- list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1"), c("bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+"))
+USM <- list(c(),
+            list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1"), c("bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+")), 
+            c(),
+            list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1"), c("bo96iN+", "lu96iN+"), c("lu96iN6", "lu97iN+")),
+            list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1", "bo96iN+", "lu96iN+", "lu96iN6"), c("lu97iN+")))
 
-# Paramètre qui change selon les USM est toujours mis en dernier
-# Maximum un seul paramètre qui change selon les USM
+
+#TEST 2
 param_name <- c("dlaimax", "durvieF")
-lb <- c(0.0005, 50)
-ub <- c(0.0025, 400)
+lb <- list(0.0005, c(50, 40)) 
+ub <- list(0.0025, c(400, 500))
+# Tous les USM
+USM <- list(c(),
+            list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1"), c("bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+")))
 
 
+lb_vec <- unlist(lb)
+ub_vec <- unlist(ub)
 
-USM_list <- vector("character")
-for (i in 1:length(USM)){
-  USM_list <- append(USM_list, USM[[i]])
+
+#USM_list <- vector("character")
+#for (i in 1:length(USM)){
+#if (is.list(USM[[i]])==TRUE){
+#for (y in 1:length(USM[[i]]))
+#USM_list <- append(USM_list, USM[[i]][[y]])
+#}
+#}
+
+USM_list <- unlist(USM)
+
+if (TRUE %in% duplicated(USM_list)){
+  doublons<-which(duplicated(USM_list))
+  USM_list<-USM_list[-doublons]
 }
 
-param_value <- c(runif(1,lb[1],ub[1]), runif(1,lb[2],ub[2]))
+
+
+# Valeurs aléatoires des paramètres
+param_value <- list()
+for (i in 1:length(USM)){
+  inter <- vector("numeric")
+  if (is.null(USM[[i]])==TRUE){
+    param_value <- append(param_value, runif(1,lb[[i]],ub[[i]]))
+  } else {
+    for (y in 1:length(USM[[i]])){
+      inter <- append(inter, runif(1,lb[[i]][y],ub[[i]][y]))
+    }
+    param_value <- append(param_value, list(inter))
+  }
+}
+
+param_value_vec <- unlist(param_value)
 
 # Pas réussi à le faire sans le "for"
-for (i in 1:(length(USM)-1)){
-  param_name <- append(param_name, param_name[length(param_name)]) 
-  lb <- append(lb, lb[length(lb)]) 
-  ub <- append(ub, ub[length(ub)]) 
-  param_value <- append(param_value, runif(1,lb[length(lb)],ub[length(ub)])) 
-}
+#for (i in 1:(length(USM)-1)){
+#param_name <- append(param_name, param_name[length(param_name)]) 
+#lb <- append(lb, lb[length(lb)]) 
+#ub <- append(ub, ub[length(ub)]) 
+#param_value <- append(param_value, runif(1,lb[length(lb)],ub[length(ub)])) 
+#}
 
 val_removed <- -999.00
 flag_log <- FALSE
@@ -122,7 +170,6 @@ for (a in 1:length(USM_list)) {
   
 }
 
-nloptr(x0 = param_value, eval_f = Stic_Crit, lb = lb, ub = ub, opts = list("algorithm"="NLOPT_LN_NELDERMEAD", "xtol_rel"=1e-08, "maxeval"=10), 
+nloptr(x0 = param_value_vec, eval_f = Stic_Crit, lb = lb_vec, ub = ub_vec, opts = list("algorithm"="NLOPT_LN_NELDERMEAD", "xtol_rel"=1e-08, "maxeval"=10), 
        param_name = param_name, USM = USM, USM_list = USM_list, obs_val = all_obs, obs_jul = all_jul_obs, flag_log = flag_log)
-
-
+}
