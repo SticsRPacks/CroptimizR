033ffb2ff3ad1a06c8fce8a83469cc927af77fe4
sbuis <samuel.buis@inra.fr>
2019-12-12 16:14:02 +0100

    Grouped the criterion and their doc in one file

---
+++ R/ls_criterion.R
@@ -0,0 +1,76 @@
+#' @title Criterion to optimize
+#'
+#' @description
+#' Provide several least squares criterion to estimate parameters by minimizing the 
+#' difference between observed and simulated values of model output variables.
+#'
+#' @param sim_list List of simulated variables
+#' @param obs_list List of observed variables
+#' 
+#' @return The value of the criterion given the observed and simulated values of the variables.
+#'
+#' @details The following criterion are proposed ([see html version](https://sticsrpacks.github.io/SticsOptimizR/reference/index.html) for a better rendering of equations):
+#' \itemize{
+#'   \item `concentrated_wss`: concentrated version of weighted sum of squares
+#'           \deqn{ \prod_{j} {(\frac{1}{n_j} \sum_{i,k} [Y_{ijk}-f_{jk}(X_i;\theta)]^2 )} }
+#'           where \eqn{ Y_{ijk} } is the observed value for the \eqn{k^{th}} time point of the \eqn{j^{th}} variable in the \eqn{i^{th}}
+#'           situation,
+#'           \eqn{ f_{jk}(X_i;\theta) } the corresponding model prediction, and \eqn{n_j} the number of measurements of variable \eqn{j}. \cr
+#'           More details about this criterion are given in Wallach et al. (2011), equation 5.
+#'   \item `log_concentrated_wss`: log transformation of concentrated version of weighted sum of squares \cr
+#'           This criterion can be useful in place of concentrated_wss if the sum of residues are null for a given situation 
+#'           (this may happen for example when one optimize on integers such as phenological stages days ...)
+#' }
+#' `sim_list` and `obs_list` must have the same structure (i.e. same number of variables, dates, situations, ... use internal function 
+#' intersect_sim_obs before calling the criterion functions).
+#'
+#' @name ls_criterion
+#'
+NULL
+
+
+#' @export
+#' @rdname ls_criterion
+concentrated_wss <- function(sim_list,obs_list) {
+
+  var_list=unique(unlist(lapply(obs_list,function (x) colnames(x))))
+  var_list=setdiff(var_list,"Date")
+
+  result=1
+
+  for (var in var_list) {
+    obs=unlist(sapply(obs_list,function (x) x[is.element(colnames(x),var)]))
+    sim=unlist(sapply(sim_list,function (x) x[is.element(colnames(x),var)]))
+    res=obs-sim
+    res=res[!is.na(res)]
+
+    sz=length(res)
+
+    result=result *((1/sz)*(res%*%res))^(sz/2)
+  }
+
+  return(result)
+}
+
+#' @export
+#' @rdname ls_criterion
+log_concentrated_wss <- function(sim_list,obs_list) {
+
+  var_list=unique(unlist(lapply(obs_list,function (x) colnames(x))))
+  var_list=setdiff(var_list,"Date")
+
+  result=0
+
+  for (var in var_list) {
+    obs=unlist(sapply(obs_list,function (x) x[is.element(colnames(x),var)]))
+    sim=unlist(sapply(sim_list,function (x) x[is.element(colnames(x),var)]))
+    res=obs-sim
+    res=res[!is.na(res)]
+
+    sz=length(res)
+
+    result=result + (sz/2)*(log(1/sz)+log(res%*%res+1e-300))
+  }
+
+  return(result)
+}
