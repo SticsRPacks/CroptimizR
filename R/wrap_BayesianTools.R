a987a3ac53b0233525681e6702b830eb2a174216
Samuel Buis <samuel.buis@inrae.fr>
2020-01-10 17:59:15 +0100

    FIX: gelman.plot needs a sufficient number of iterations to be created

--- R/wrap_BayesianTools.R
+++ R/wrap_BayesianTools.R
@@ -15,12 +15,29 @@ wrap_BayesianTools <- function(param_names,obs_list,crit_function,model_function
 
   if (is.null(optim_options$iterations)) { optim_options$iterations=10000 }
   if (is.null(optim_options$startValue)) { optim_options$startValue=5 }
+  if (is.null(optim_options$thin)) { optim_options$thin=1 }
   if (is.null((ranseed=optim_options$ranseed))) { ranseed=NULL }
   if (is.null((path_results=optim_options$path_results))) { path_results=getwd() }
 
   optim_options_tmp=list()
   optim_options_tmp$iterations=optim_options$iterations
   optim_options_tmp$startValue=optim_options$startValue
+  optim_options_tmp$thin=optim_options$thin
+
+  nb_chains=optim_options$startValue
+  if ( is.numeric(nb_chains) && (length(nb_chains)==1 || dim(nb_chains)==2) ) {
+    if (length(nb_chains)==1) {
+      nb_chains=optim_options$startValue
+    } else {
+      nb_chains=nrow(optim_options$startValue)
+    }
+  } else {
+    stop("optim_options$startValue must be a numeric (number of chains or a matrix of parameters initial values")
+  }
+
+  # number of iterations (optim_options$iterations is the total number of evaluation, i.e. nb_chains*nb_iterations)
+  nb_iterations=ceiling(optim_options$iterations/nb_chains)
+
 
   set.seed(ranseed)
 
@@ -49,7 +66,6 @@ wrap_BayesianTools <- function(param_names,obs_list,crit_function,model_function
   }
 
   # Perform the Bayesian analysis
-  settings = list(iterations = 10000, startValue=5)
   out <- runMCMC(bayesianSetup = bayesianSetup, sampler = "DREAMzs",
                  settings = optim_options_tmp)
 
@@ -70,13 +86,19 @@ wrap_BayesianTools <- function(param_names,obs_list,crit_function,model_function
 
   if (nb_params>=2) {
     # seems that it does not work for a single parameter
-    grDevices::pdf(file = file.path(path_results,"gelmanDiagPlots.pdf") , width = 9, height = 9)
-    gelmanDiagnostics(out, thin="auto", plot = T)
-    grDevices::dev.off()
+    # also, Nbiteration must be > thin+50 otherwise coda::gelman.plot end with an error
+    if ( nb_iterations>=(optim_options_tmp$thin+50) ) {
+      grDevices::pdf(file = file.path(path_results,"gelmanDiagPlots.pdf") , width = 9, height = 9)
+      gelmanDiagnostics(out, thin=optim_options_tmp$thin, plot = T)
+      grDevices::dev.off()
+    } else {
+      gelmanDiagnostics(out, thin=optim_options_tmp$thin, plot = F)
+      warning("Number of iterations in DREAM is too low to generate gelmanDiagPlots.pdf (should be superior to thin+50)")
+    }
   }
 
   # Print results
-  codaObject = getSample(out, start = 500, coda = TRUE)
+  codaObject = getSample(out, start = 1, coda = TRUE)  # thin=1
   tmp=summary(codaObject)
   if (nb_params>=2) {
     summary(out) }
