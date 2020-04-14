1b8e9787562b77a3ebfd56c0aa467b8decaa7529
sbuis <samuel.buis@inra.fr>
2020-04-14 14:34:13 +0200

    Rewrote the vignettes to simplify automatic tests

--- vignettes/Parameter_estimation_Stics_install.R
+++
@@ -1,8 +0,0 @@
-
-#' ## Initialisation step
-#'
-## ----setup_initializations,  message=FALSE, results=FALSE, warning=FALSE----
-
-# Install and load the needed libraries
-devtools::install_github("SticsRPacks/SticsRPacks")
-library("SticsRPacks")
--- vignettes/Parameter_estimation_Stics_path_stics.R
+++
@@ -1,4 +0,0 @@
-
-# DEFINE THE PATH TO YOUR LOCALLY INSTALLED VERSION OF JAVASTICS
-javastics_path=file.path(path_to_JavaStics,"JavaSTICS-1.41-stics-9.0")
-stics_path=file.path(javastics_path,"bin/stics_modulo.exe")
