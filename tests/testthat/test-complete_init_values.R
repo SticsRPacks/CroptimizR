7629f9ce4900b015a39ec652c65f2b05bacc5c34
Samuel Buis <samuel.buis@inra.fr>
2021-04-22 11:15:47 +0200

    Modified the tests for sampling methods
    no more based on snapshot since lhs package does not give the same values on different platforms ...

--- tests/testthat/test-complete_init_values.R
+++ tests/testthat/test-complete_init_values.R
@@ -10,15 +10,16 @@ satisfy_par_const = function(param_values, ...) {
   return(TRUE)
 }
 test_that("Initial values and constraints", {
-  local_edition(3)
   res1 <- CroptimizR:::complete_init_values(init_values=NULL, nb_values=5, lb=lb, ub=ub,
                                     ranseed=1234)
   res2 <- CroptimizR:::complete_init_values(init_values=init_values, nb_values=5, lb=lb, ub=ub,
                                            ranseed=1234)
-  res3 <- CroptimizR:::complete_init_values(init_values=init_values, nb_values=5, lb=lb, ub=ub,
+  res3 <- CroptimizR:::complete_init_values(init_values=init_values, nb_values=100, lb=lb, ub=ub,
                                           satisfy_par_const=satisfy_par_const,
                                            ranseed=1234)
-  expect_snapshot(res1)
-  expect_snapshot(res2)
-  expect_snapshot(res3)
+  expect_false(isTRUE(all.equal(res1,res2)))
+  expect_equal(res2$dlaimax[1],init_values$dlaimax[1])
+  expect_equal(res2$durvieF[1:2],init_values$durvieF[1:2])
+  expect_true(all(sapply(1:nrow(res3), function(x) satisfy_par_const(res3[x,]))))
+
 })
--- tests/testthat/test-sample_params.R
+++ tests/testthat/test-sample_params.R
@@ -1,11 +1,10 @@
 bounds_list <- list(lb = c(0, 1, 2), ub = c(1, 2, 3))
-res1=CroptimizR:::sample_params(bounds_list, 5)
+res1=CroptimizR:::sample_params(bounds_list, 5, seed = 1234)
 Sys.sleep(2)
 res2=CroptimizR:::sample_params(bounds_list, 5)
+res3=CroptimizR:::sample_params(bounds_list, 5, seed = 1234)
 
 test_that("sample_params", {
-  local_edition(3)
   expect_false(isTRUE(all.equal(res1,res2)))
-  res <- CroptimizR:::sample_params(bounds_list, 2,seed=2)
-  expect_snapshot(res)
+  expect_identical(res1,res3)
 })
