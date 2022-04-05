5ba40f798b5198db65e7ee33a237787305b9c877
Samuel Buis <samuel.buis@inra.fr>
2022-04-05 15:53:06 +0200

    Added get_params_init_values as a deprecated function

--- R/param_info_functions.R
+++ R/param_info_functions.R
@@ -220,6 +220,58 @@ set_init_values <- function(param_info, init_values) {
 
 #' @title Extract parameter initial values from parameter information
 #'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @examples
+#'\donttest{
+#' # Simple cases
+#' param_info=list(init_values=c(dlaimax=0.001, durvieF=200),
+#'                        lb=c(dlaimax=0.0001, durvieF=50),
+#'                        ub=c(dlaimax=0.01, durvieF=400))
+#' CroptimizR:::get_params_init_values(param_info)
+#'
+#' param_info=list(init_values=data.frame(dlaimax=c(0.001,0.002), durvieF=c(50,200)),
+#'                        lb=c(dlaimax=0.0001, durvieF=50),
+#'                        ub=c(dlaimax=0.01, durvieF=400))
+#' CroptimizR:::get_params_init_values(param_info)
+#' # ->
+#' CroptimizR:::get_init_values(param_info)
+#'
+#' # Cases with groups of situations per parameter
+#' param_info=list()
+#' param_info$dlaimax=list(sit_list=list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1",
+#'                                                "bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+")),
+#'                                init_values=0.001,lb=0.0001,ub=0.1)
+#' param_info$durvieF=list(sit_list=list(c("bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+"),
+#'                                              c("bou99t3", "bou00t3", "bou99t1", "bou00t1")),
+#'                                init_values=c(200,300),lb=50,ub=400)
+#' CroptimizR:::get_params_init_values(param_info)
+#' # ->
+#' CroptimizR:::get_init_values(param_info)
+#'
+#' param_info=list()
+#' param_info$dlaimax=list(sit_list=list(c("bou99t3", "bou00t3", "bou99t1", "bou00t1",
+#'                                                "bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+")),
+#'                                init_values=c(0.001,0.002),lb=0.0001,ub=0.1)
+#' param_info$durvieF=list(sit_list=list(c("bo96iN+", "lu96iN+", "lu96iN6", "lu97iN+"),
+#'                                              c("bou99t3", "bou00t3", "bou99t1", "bou00t1")),
+#'                                init_values=data.frame(c(200,300),c(250,350)),lb=50,ub=400)
+#' CroptimizR:::get_params_init_values(param_info)
+#' # ->
+#' CroptimizR:::get_init_values(param_info)
+#'}
+#'
+#' @keywords internal
+#'
+get_params_init_values = function(param_info) {
+  lifecycle::deprecate_warn("0.5.0", "get_params_init_values()", "get_init_values()")
+  return(get_init_values(param_info))
+}
+
+
+#' @title Extract parameter initial values from parameter information
+#'
 #' @inheritParams estim_param
 #'
 #' @return A dataframe containing initial values for the different parameters to
