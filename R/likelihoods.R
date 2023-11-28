0aed9a12de8339175b4a6a24863398b66c16c910
Samuel Buis <samuel.buis@inra.fr>
2023-11-28 10:31:04 +0100

    Remove likelihood iidn, doc and check to be improved ...
    ... will be done for further version of the package

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -35,8 +35,8 @@
 #'           variable in the same situation are assumed to be highly correlated.
 #'           In this way, each situation contributes a single term to the
 #'           overall sum of squared errors regardless of the number of
-#'           observations which may be usefull in case one have situations with
-#'           very heterogenous number of dates of observations.
+#'           observations which may be useful in case one have situations with
+#'           very heterogeneous number of dates of observations.
 #' }
 #' `sim_list` and `obs_list` must have the same structure
 #' (i.e. same number of variables, dates, situations, ... use internal function
@@ -45,55 +45,6 @@
 #' @name Likelihoods
 #'
 NULL
-
-
-#' @export
-#' @rdname Likelihoods
-likelihood_log_iidn <- function(sim_list,obs_list, weight) {
-  #'
-  #' @description
-  #' log-likelihood for heteroscedastic and independant normal errors, errors are defined in a user function
-  #'
-  #' @param sim_list List of simulated variables
-  #' @param obs_list List of observed variables
-  #' @param weight function that takes in input a vector of observed values and the name of the correpsonding variable and that returns the corresponding error standard deviations
-  #'
-  #' @return The value of the likelihood given the observed and simulated values of the variables.
-  #'
-  #' @details
-  #'           \deqn{ \sum_{j} {\left[ \sum_{i} \left(-\frac{n_{ij}.log(2pi)}{2} -  \sum_{k} \left[log(\sigma_{ijt}) + \frac{1}{2} \left(\frac{Y_{ijk}-f_{jk}(X_i;\theta)}{\sigma_{ijt}}\right)^2 \right] \right) \right] }}
-  #'           where \eqn{ Y_{ijk} } is the observed value for the \eqn{k^{th}} time point of the \eqn{j^{th}} variable in the \eqn{i^{th}}
-  #'           situation,
-  #'           \eqn{ f_{jk}(X_i;\theta) } the corresponding model prediction, and \eqn{n_{ij}} the number of measurements of variable \eqn{j} for situation \eqn{i}. \cr
-  #'
-  #' `sim_list` and `obs_list` must have the same structure (i.e. same number of variables, dates, situations, ... use internal function
-  #' intersect_sim_obs before calling the criterion functions).
-
-  if (!nargs()) {
-    return("log-likelihood")
-  }
-
-  var_list=unique(unlist(lapply(obs_list,function (x) colnames(x))))
-  var_list=setdiff(var_list,"Date")
-
-  result=0
-
-  for (var in var_list) {
-    obs <- unlist(sapply(obs_list, function(x) x[is.element(colnames(x), var)]))
-    sim <- unlist(sapply(sim_list, function(x) x[is.element(colnames(x), var)]))
-    res <- obs - sim
-    id_not_na <- !is.na(res)
-    res <- res[id_not_na]
-    sigma <- weight(obs[id_not_na], var)
-
-    sz <- length(res)
-
-    result=result - 0.5*sz*log(2*pi)-sum(log(sigma))-0.5*((res/sigma)%*%(res/sigma))
-
-  }
-
-  return(as.numeric(result))
-}
 
 
 #' @export
