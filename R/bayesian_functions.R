9d9e2915d77f4d201b6ab2f2cbaa55fdeaa1f4d9
Rémi Vezy <VEZY@users.noreply.github.com>
2025-02-26 16:41:19 +0100

    test styler ga

--- R/bayesian_functions.R
+++ R/bayesian_functions.R
@@ -7,7 +7,7 @@
 #' @return Prints results of bayesian methods
 #' @keywords internal
 #'
-summary_bayesian <- function(optim_options, param_info, optim_results) {
+summary_bayesian = function(optim_options, param_info, optim_results) {
   param_names <- get_params_names(param_info)
   nb_params <- length(param_names)
   path_results <- optim_options$path_results
