388bae8261db94d9db46437c16a18dc24a3bc6bf
Samuel Buis <samuel.buis@inra.fr>
2025-05-28 18:19:03 +0200

    Fixed import of where function and updated doc

--- R/filter_obs.R
+++ R/filter_obs.R
@@ -18,6 +18,7 @@
 #' @export
 #'
 #' @importFrom rlang .data
+#' @importFrom dplyr where
 #'
 #' @examples
 #'
@@ -164,7 +165,7 @@ filter_obs <- function(obs_list, var = NULL, situation = NULL, dates = NULL,
   obs_list <- lapply(
     obs_list,
     function(x) {
-      select(x, !.data$id & tidyselect::where(~ !all(is.na(.x))))
+      select(x, !.data$id & dplyr::where(~ !all(is.na(.x))))
     }
   )
 
